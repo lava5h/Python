70233e836ad9f5d6e28eb4135c8cbf5f9a9ba843
lava5h <lav45h@yandex.ru>
2025-02-17 14:26:36 +0700

    DZ3

---
+++ lesson_03/lesson_3_task_2.py
@@ -0,0 +1,12 @@
+from smartphone import Smartphone
+
+catalog = []
+
+catalog.append(Smartphone("Apple", "iPhone 11", "+79123456789"))
+catalog.append(Smartphone("Apple", "iPhone 12", "+79234567890"))
+catalog.append(Smartphone("Apple", "iPhone 13", "+79345678901"))
+catalog.append(Smartphone("Apple", "iPhone 14", "+79456789012"))
+catalog.append(Smartphone("Apple", "iPhone 15", "+79567890123"))
+
+for smartphone in catalog:
+    print(f"{smartphone.phone_brand} - {smartphone.phone_model}. {smartphone.phone_number}")
