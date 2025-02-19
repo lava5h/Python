b32ff343dd94f83c80a1aaedb17a71b0964ff7b0
lava5h <lav45h@yandex.ru>
2025-02-19 21:41:13 +0700

    DZ4

--- lesson_04/test_string_utils.py
+++ lesson_04/test_string_utils.py
@@ -23,3 +23,58 @@ def test_capitalize_positive(input_str, expected):
 ])
 def test_capitalize_negative(input_str, expected):
     assert string_utils.capitalize(input_str) == expected
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, expected", [
+    (" Skypro", "Skypro"),
+    ("  Hello world", "Hello world"),
+    ("   Python", "Python"),
+])
+def test_trim_positive(input_str, expected):
+    assert string_utils.trim(input_str) == expected
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, expected", [
+    ("Skypro ", "Skypro "),
+])
+def test_trim_negative(input_str, expected):
+    assert string_utils.trim(input_str) == expected
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("Skypro", "S", True),
+    ("Skypro", "U", False),
+    ("Skypro", "y", True),
+])
+def test_contains_positive(input_str, symbol, expected):
+    assert string_utils.contains(input_str, symbol) == expected
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("Skypro", "", True),
+    ("", "", True),
+])
+def test_contains_negative(input_str, symbol, expected):
+    assert string_utils.contains(input_str, symbol) == expected
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("Skypro", "S", "kypro"),
+    ("Skypro", "Sky", "pro"),
+])
+def test_delete_symbol_positive(input_str, symbol, expected):
+    assert string_utils.delete_symbol(input_str, symbol) == expected
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, symbol, expected", [
+    ("Skypro", "", "Skypro"),
+    ("", "a", ""),
+])
+def test_delete_symbol_negative(input_str, symbol, expected):
+    assert string_utils.delete_symbol(input_str, symbol) == expected
