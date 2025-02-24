0ddda7d06211e3a9066e35494777a27daf4f6d5a
lava5h <lav45h@yandex.ru>
2025-02-25 02:19:34 +0700

    DZ5

---
+++ lesson_05/3.py
@@ -0,0 +1,17 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+# Открываем страницу
+driver.get("http://uitestingplayground.com/classattr")
+
+# Кликаем на синюю кнопку
+button = driver.find_element(By.CSS_SELECTOR, ".btn.btn-primary")
+button.click()
+
+sleep(5)
